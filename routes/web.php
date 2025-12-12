d46698fb6fe0a3edf06e615d5d75ca5d95400fa2
ElcoTerwoert <elco.terwoert@gmail.com>
2025-12-12 14:03:02 +0100

    added migrations according to ERD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -14,6 +15,19 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+
+Route::get('/test-users', function () {
+    // Haal alle gebruikers op (zonder wachtwoord)
+    $users = DB::table('users')->select('id', 'name', 'email')->get();
+
+    // Geef de gebruikers terug als JSON
+    return response()->json($users);
+});
+
+Route::get('/db-test', function () {
+    return DB::connection()->getDatabaseName();
 });
 
 require __DIR__.'/settings.php';
