57b406111746b9a380b85a4dfc2f914be1ba1b60
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2026-01-09 14:26:26 +0100

    End of day push

--- GeoProfs/obj/Debug/net8.0/GeoProfs.AssemblyInfo.cs
+++ GeoProfs/obj/Debug/net8.0/GeoProfs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeoProfs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c120f0b54f3e141ce6596fadb0a26cf4f9cf032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa0ca667bc093471f04cfd217c817bc7862e186")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeoProfs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeoProfs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
